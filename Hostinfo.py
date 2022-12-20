55c45a4333452e627ab76ca1a5d7cb42323a7fef
Isah Bashir Ibrahim <get2bash@gmail.com>
2022-12-20 08:48:59 +0100

    Create Hostinfo.py

---
+++ Hostinfo.py
@@ -0,0 +1,12 @@
+ Display hostname andIP address
+import socket
+def host_IP():
+   try:
+      hname = socket.gethostname()
+      hip = socket.gethostbyname(hname)
+      print("Hostname:  ",hname)
+      print("IP Address: ",hip)
+   except:
+      print("Unable to get Hostname and IP")
+# Driver code
+host_IP() #Function call
