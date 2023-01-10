51c45061bd7bce566385f5fcdae633d20a7315c7
Isah Bashir Ibrahim <get2bash@gmail.com>
2023-01-10 23:34:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ if __name__ == '__main__':
  
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run(host="0.0.0.0",debug=true)
+    app.run(host="0.0.0.0",debug=True)
