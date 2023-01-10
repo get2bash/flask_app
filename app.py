dbd7e17f2589781e277c42c4614aed82eecaf1c1
Isah Bashir Ibrahim <get2bash@gmail.com>
2023-01-10 22:45:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from flask import Flask
+import socket
+
+
+app = Flask(__name__)
+
+@app.route('/')
 # Function to display hostname and IP address
 def get_Host_name_IP():
 	try:
@@ -7,6 +14,9 @@ def get_Host_name_IP():
 		print("IP : ", host_ip)
 	except:
 		print("Unable to get Hostname and IP")
-
-# Function call
-get_Host_name_IP()
+		
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(host="0.0.0.0",debug=true)
